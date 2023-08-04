b98423050f236a4efe187c142e6066d8c97fe2af
nicolearnieri <nicolearnieri@gmail.com>
2023-08-04 22:38:09 +0200

    Temini

--- src/main/java/uid/project/deliverboo/HelloApplication.java
+++ src/main/java/uid/project/deliverboo/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/SceneBuilder/PrimaInterfaccia.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 600);
         stage.setTitle("Hello!");
-        String css = getClass().getResource("/css/LightMode.css").toExternalForm();
+        String css = getClass().getResource("/css/Light2.css").toExternalForm();
         scene.getStylesheets().add(css);
         stage.setScene(scene);
         stage.show();
