8bc231067a85a05d9a3176c2fe3ac302a6e013eb
valen <valentinaricioppo@gmail.com>
2023-08-09 13:52:53 +0200

    faqfxml

--- src/main/java/uid/project/deliverboo/HelloApplication.java
+++ src/main/java/uid/project/deliverboo/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/SceneBuilder/SearchRestaurants.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
         stage.setTitle("DeliverBoo");
         String css = getClass().getResource("/css/LightTheme.css").toExternalForm();
         scene.getStylesheets().add(css);
