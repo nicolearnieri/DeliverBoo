76ee68df61bc2e1f27efddf82aca0b144b2de17e
nicolearnieri <nicolearnieri@gmail.com>
2023-08-13 18:54:30 +0200

    temi

--- src/main/java/uid/project/deliverboo/model/CurrentUser.java
+++ src/main/java/uid/project/deliverboo/model/CurrentUser.java
@@ -2,14 +2,12 @@ package uid.project.deliverboo.model;
 
 public class CurrentUser
 {
+    private static  CurrentUser instance;
     private String nomeUtente;
     private String email;
+    private boolean access = false;
 
-    public CurrentUser(String nomeUtente, String email)
-    {
-        this.nomeUtente = nomeUtente;
-        this.email= email;
-    }
+    private CurrentUser() {}
 
     public String getNomeUtente() {return nomeUtente;}
 
@@ -17,4 +15,20 @@ public class CurrentUser
     public String getEmail() {return email;}
 
     public void setEmail(String email) {this.email = email;}
+
+    public boolean getAccess() { return access; }
+
+    public void setAccess(boolean value) {this.access = value;}
+
+    public static CurrentUser getInstance() {
+        if (instance == null)
+            instance = new CurrentUser();
+            return instance;
+    }
+
+    public void logOut ()
+    {
+        this.email = null;
+        this.nomeUtente = null;
+        this.access= false; }
 }
