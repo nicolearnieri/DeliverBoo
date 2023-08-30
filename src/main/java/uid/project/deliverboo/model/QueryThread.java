8c4eff01437dc9444817c2b2a423fd60ac7de9a0
nicolearnieri <nicolearnieri@gmail.com>
2023-08-30 23:29:25 +0200

    Thread in palestra

--- src/main/java/uid/project/deliverboo/model/QueryThread.java
+++ src/main/java/uid/project/deliverboo/model/QueryThread.java
@@ -1,6 +1,30 @@
 package uid.project.deliverboo.model;
 
-public class QueryThread extends Thread{
+import javafx.concurrent.Task;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+class QueryThread extends Thread {
+
+    private BlockingQueue<Runnable> commandQueue = new LinkedBlockingQueue<>(); //conterrà i comandi che il thread dovrà eseguire
+
     @Override
-    public void run() {}
+    public void run() {
+        try {
+            while (true)
+            {
+                Runnable command = commandQueue.take(); // Prende il prossimo comando dalla coda, poi lo esegue
+                command.run();
+            }
+        }
+        catch (InterruptedException e)
+        {// Thread interrotto
+        }
+
+    }
+    // Metodo per aggiungere un nuovo comando alla coda
+    public void addCommand(Runnable command) {
+        commandQueue.add(command);
+    }
 }
