e28c3fbd004ac17fe0beef167bd15efd87f78354
valen <valentinaricioppo@gmail.com>
2023-09-05 17:27:39 +0200

    applausi per vale

--- src/main/java/uid/project/deliverboo/controller/RestaurantHomeController.java
+++ src/main/java/uid/project/deliverboo/controller/RestaurantHomeController.java
@@ -1,11 +1,13 @@
 package uid.project.deliverboo.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import uid.project.deliverboo.model.Restaurant;
 import uid.project.deliverboo.view.Cart;
 import uid.project.deliverboo.view.MenuItem;
@@ -17,7 +19,10 @@ import java.util.Objects;
 public class RestaurantHomeController {
 
     @FXML
-    private ImageView backButton;
+    private Button backButton;
+
+    @FXML
+    private ImageView backButtonImg;
 
     @FXML
     private AnchorPane cartSpace;
@@ -33,19 +38,32 @@ public class RestaurantHomeController {
 
     private Cart cart;
 
-    public void initialize(Restaurant restaurant){
+    private Stage searchRestaurant;
+
+    private Stage ownStage;
+
+    public void initialize(Restaurant restaurant, Stage stage, Stage secondStage){
+        this.searchRestaurant=stage;
+        this.ownStage=secondStage;
         cart=new Cart();
         cart.loadCart();
+        stage.hide();
         /*for(qua va fatta scorrere la lista della degli oggetti del menu){
             //qua vanno creati new MenuItem
         }*/
         restaurantNameLabel.setText(restaurant.getName());
-        imageRestaurant.setImage(new Image(Objects.requireNonNull(getClass().getResource(restaurant.getPath2())).toExternalForm()));
+        //imageRestaurant.setImage(new Image(Objects.requireNonNull(getClass().getResource(restaurant.getPath2())).toExternalForm()));
 
 
 
 
 
+    }
+
+    public void getBack(){
+        //qua va aggiunto l'if se la list del cart è vuoto
+        ownStage.close();
+        searchRestaurant.show();
     }
 
 }
