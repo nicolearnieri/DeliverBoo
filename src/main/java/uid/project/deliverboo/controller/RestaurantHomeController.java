333afeb929178c9578befd128013eeeedad248c3
valen <valentinaricioppo@gmail.com>
2023-09-06 17:38:13 +0200

    w le caprette

--- src/main/java/uid/project/deliverboo/controller/RestaurantHomeController.java
+++ src/main/java/uid/project/deliverboo/controller/RestaurantHomeController.java
@@ -8,12 +8,20 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import uid.project.deliverboo.model.ExecutorProvider;
+import uid.project.deliverboo.model.Food;
 import uid.project.deliverboo.model.Restaurant;
+import uid.project.deliverboo.model.TaskCreator;
 import uid.project.deliverboo.view.Cart;
 import uid.project.deliverboo.view.MenuItem;
 import uid.project.deliverboo.view.SceneHandler;
 
 import java.util.Objects;
+import java.util.Vector;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 
 public class RestaurantHomeController {
@@ -31,7 +39,7 @@ public class RestaurantHomeController {
     private ImageView imageRestaurant;
 
     @FXML
-    private ListView<?> menuList;
+    private ListView<MenuItem> menuList;
 
     @FXML
     private Label restaurantNameLabel;
@@ -42,18 +50,48 @@ public class RestaurantHomeController {
 
     private Stage ownStage;
 
-    public void initialize(Restaurant restaurant, Stage stage, Stage secondStage){
+    private static Vector<Food> menu = new Vector<>();
+    private ExecutorService executor = ExecutorProvider.getExecutor();
+
+    public void initialize(Restaurant restaurant, Stage stage, Stage secondStage, LocalizationManager localizationManager){
         this.searchRestaurant=stage;
         this.ownStage=secondStage;
         cart=new Cart();
         cart.loadCart();
         stage.hide();
-        /*for(qua va fatta scorrere la lista della degli oggetti del menu){
-            //qua vanno creati new MenuItem
-        }*/
         restaurantNameLabel.setText(restaurant.getName());
+
+        System.out.println("pos1");
         //imageRestaurant.setImage(new Image(Objects.requireNonNull(getClass().getResource(restaurant.getPath2())).toExternalForm()));
 
+        Callable<Boolean> verifyCallable = TaskCreator.ReturnFoodInfoCallable(restaurant.getCode());
+
+        Future<Boolean> result = executor.submit(verifyCallable);
+        try {
+            System.out.println("Controller try");
+            Boolean flag = result.get();
+        } catch (InterruptedException e) {
+            System.out.println("Controller catch1");
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            System.out.println("Controller catch2");
+            throw new RuntimeException(e);
+        }
+
+
+        System.out.println("prima di creare il menu");
+        menuList.getItems().clear();
+        for(Food food: menu){
+            System.out.println("crea menuItem");
+            MenuItem menuItem= new MenuItem(food, localizationManager);
+            System.out.println("aggiunge a menuList");
+            menuList.getItems().add(menuItem);
+            for(MenuItem menuIt: menuList.getItems()){
+                System.out.println(menuIt);
+            }
+        }
+
+        menuList.refresh();
 
 
 
@@ -64,6 +102,14 @@ public class RestaurantHomeController {
         //qua va aggiunto l'if se la list del cart è vuoto
         ownStage.close();
         searchRestaurant.show();
+    }
+
+    public static boolean addToVector(Food food) {
+        System.out.println("Aggiunge al menu");
+        for(Food cibo: menu){
+            System.out.println(cibo);
+        }
+        return menu.add(food);
     }
 
 }
--- src/main/java/uid/project/deliverboo/view/MenuItem.java
+++ src/main/java/uid/project/deliverboo/view/MenuItem.java
@@ -2,15 +2,19 @@ package uid.project.deliverboo.view;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import uid.project.deliverboo.controller.LocalizationManager;
 import uid.project.deliverboo.controller.MenuItemController;
+import uid.project.deliverboo.model.Food;
 
 public class MenuItem extends StackPane {
-    public MenuItem(){
-        FXMLLoader loader=new FXMLLoader(MenuItem.class.getResource("/SceneBuilder/MenuItem.fxml"));
+    public MenuItem(Food food, LocalizationManager localizationManager){
+        FXMLLoader loader=new FXMLLoader(MenuItem.class.getResource("/SceneBuilder/ProductOrder.fxml"));
         try {
-            ListView root= loader.load();
+            BorderPane root= loader.load();
             MenuItemController controller=loader.getController();
+            controller.init(food, localizationManager);
             this.getChildren().add(root);
 
         }catch (Exception ignoredExeption){}
