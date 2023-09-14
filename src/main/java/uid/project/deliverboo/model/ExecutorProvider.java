ebbde3cecd47a0ec1fda97ec7dc48ba8583749be
nicolearnieri <nicolearnieri@gmail.com>
2023-09-14 15:21:33 +0200

    3 4 cvv

--- src/main/java/uid/project/deliverboo/model/ExecutorProvider.java
+++ src/main/java/uid/project/deliverboo/model/ExecutorProvider.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ExecutorProvider {
-    private static final ExecutorService executor = Executors.newFixedThreadPool(1);
+    private static final ExecutorService executor = Executors.newFixedThreadPool(2);
 
     public static ExecutorService getExecutor() {
         return executor;
